66fc5fe930ace80e136070904a5fbf4375b5cfb5
Michele Scuttari <mscuttari@users.noreply.github.com>
2020-07-10 15:43:48 +0200

    v1.0.0-beta.7

--- test/src/test/java/it/mscuttari/examples/AbstractTest.java
+++ test/src/test/java/it/mscuttari/examples/AbstractTest.java
@@ -22,7 +22,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import java.util.Calendar;
